82bd443bb31218026d128fd92e68891d28439436
matiuskm <matius.km@gmail.com>
2025-02-23 11:36:43 +0700

    Add image selection functionality to Carousel; implement state management for selected image and update rendering logic

--- resources/js/Components/Core/Carousel.tsx
+++ resources/js/Components/Core/Carousel.tsx
@@ -1,22 +1,30 @@
 import {Image} from "@/types";
+import {useEffect, useState} from "react";
 
 function Carousel({images}: { images: Image[] }) {
+  const [selectedImage, setSelectedImage] = useState<Image>(images[0])
+
+  useEffect(() => {
+    setSelectedImage(images[0])
+  }, images);
+
   return (
     <>
       <div className="flex items-start gap-8">
         <div className="flex flex-col items-center gap-2 py-2">
-          {images.map((image, i) => (
-            <a href={'#item' + i} className="border-2 hover:border-blue-500" key={image.id}>
+          {images.map((image) => (
+            <button onClick={_ => setSelectedImage(image)}
+                    className={"border-2 " +
+                      (selectedImage.id === image.id ? 'border-blue-500' : 'hover:border-blue-500')}
+                    key={image.id}>
               <img src={image.thumb} alt="" className="w-[50px]"/>
-            </a>
+            </button>
           ))}
         </div>
         <div className="carousel w-full">
-          {images.map((image, i) => (
-            <div id={"item" + i} className="carousel-item w-full" key={image.id}>
-              <img src={image.large} alt="" className="w-full"/>
-            </div>
-          ))}
+          <div className="carousel-item w-full">
+            <img src={selectedImage.large} alt="" className="w-full"/>
+          </div>
         </div>
       </div>
     </>
